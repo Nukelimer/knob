041b7fadc955bcc4e8fb8a3e71312e77b8b9e2cb
Nukelimer <codewithremi@gmail.com>
2025-09-14 22:24:21 +0100

    fixed the nav "NEW"

--- src/components/Home/Navigation.svelte
+++ src/components/Home/Navigation.svelte
@@ -1,103 +1,157 @@
 <script lang="ts">
-  import Button from '../Resuable/Button.svelte';
-  import knob from '$lib/assets/images/Home/logo_.webp';
+	import Button from '../Resuable/Button.svelte';
+	import knob from '$lib/assets/images/Home/logo_.webp';
+	import knob_dark from '$lib/assets/images/Home/logo_dark.webp';
 
-  const urls = ['keyboard', 'keysets', 'numpad', 'lightswitch'];
-  const sub_url = ['k·no·b·sans', 'k·no·b·sidians'];
+	const urls = ['keyboard', 'keysets', 'numpad', 'lightswitch'];
+	const sub_url = ['k·no·b·sans', 'k·no·b·sidians'];
+	import { page } from '$app/stores';
+	import { twMerge } from 'tailwind-merge';
+	let showSubURL = false;
+	let hoverTimeout: ReturnType<typeof setTimeout>;
 
-  let showSubURL = false;
-  let hoverTimeout: ReturnType<typeof setTimeout>;
+	function handleEnter() {
+		hoverTimeout = setTimeout(() => {
+			showSubURL = true;
+		}, 300);
+	}
 
-  function handleEnter() {
-    hoverTimeout = setTimeout(() => {
-      showSubURL = true;
-    }, 300);
-  }
-
-  function handleLeave() {
-    clearTimeout(hoverTimeout);
-    showSubURL = false;
-  }
-
-
-
-
- 
-  
+	function handleLeave() {
+		clearTimeout(hoverTimeout);
+		showSubURL = false;
+	}
 </script>
 
 <nav class="absolute top-0 flex w-full items-center justify-between p-6">
+
+  {#if  $page.url.pathname === '/numpad' }
+<a href="/">
+
+
+  <img src={knob_dark} alt="logo" class="h-8 w-24" />
+</a>
+  
+{:else}
+
+<a href="/">
+
+
   <img src={knob} alt="logo" class="h-8 w-24" />
 
-  <ul class="flex gap-4">
-    {#each urls as url}
-      {#if url === 'keysets'}
-        <li
-          class="relative"
-          on:mouseenter={handleEnter}
-          on:focus={handleEnter}
-          on:mouseleave={handleLeave}
-          on:blur={handleLeave}
-        >
-          <button
-            class="bg text-[12px] font-light text-[#f0ece0] font-sans 
-                   hover:text-[#cdea19] hover:underline 
-                   cursor-pointer transition-all delay-300"
-          >
-            {url}
-          </button>
+</a>
 
-          {#if showSubURL}
-            <ul class="absolute -left-6 -mt-2 bg-transparent p-2 rounded shadow">
-              {#each sub_url as sub}
-                <li>
-                  <a
-                    href={"/" + encodeURIComponent(sub)}
-                    class="block px-2 py-1 text-[12px] font-light text-[#f0ece0] 
-                           hover:text-[#cdea19] hover:underline"
-                  >
-                    {sub}
-                  </a>
-                </li>
-              {/each}
-            </ul>
-          {/if}
-        </li>
+  {/if}
 
-        {:else if url == "keyboard"}
-         <li>
-          <a
-            href={"/"}
-            class="bg text-[12px] font-light text-[#f0ece0] !not-italic 
-                   hover:text-[#cdea19] hover:underline"
-          >
-            {url}
-          </a>
-        </li>
 
-      {:else}
-        <li>
-          <a
-            href={"/" + url}
-            class="bg text-[12px] font-light text-[#f0ece0] !not-italic 
-                   hover:text-[#cdea19] hover:underline"
-          >
-            {url}
-          </a>
-        </li>
-      {/if}
-    {/each}
-  </ul>
 
-  <form action="" class="hidden gap-4 md:flex">
-    <input
-      type="email"
-      name="email"
-      class="h-10 max-w-48 rounded-4xl not-italic ring-0 outline-0 
-             placeholder:text-[13px] placeholder:tracking-wider 
+	<ul class="flex gap-4">
+		{#each urls as url}
+			{#if url === 'keysets'}
+				<li
+					class="relative"
+					on:mouseenter={handleEnter}
+					on:focus={handleEnter}
+					on:mouseleave={handleLeave}
+					on:blur={handleLeave}
+				>
+					<button
+						class={twMerge(`bg cursor-pointer font-sans text-[12px] font-light 
+                   text-[#f0ece0] transition-all 
+                   delay-300 hover:text-[#cdea19] hover:underline  ${$page.url.pathname === '/numpad' ? 'text-[#000]   hover:text-[#000] ' : 'text-[#f0ece0] '}`)}
+					>
+						{url} <span class="text-[#ff4d00] uppercase font-medium">NEW</span>
+					</button>
+
+				
+
+
+
+
+
+
+
+
+
+
+          	{#if showSubURL}
+						<ul class="absolute -left-6 -mt-2 rounded bg-transparent p-2 ">
+							{#each sub_url as sub}
+
+
+
+              <li>
+              {#if sub == 'k·no·b·sans' }
+                <a
+                  href={'/' + encodeURIComponent(sub)}
+                  class={twMerge(`block px-2 py-1 text-[12px] font-light text-[#f0ece0] 
+                         hover:text-[#cdea19] hover:underline  ${$page.url.pathname === '/numpad' ? 'text-[#000]  hover:text-[#000]  ' : 'text-[#f0ece0] '}`)}
+                >
+                  {sub}
+                </a>
+                {:else}
+                
+                 <a
+                  href={'/' + encodeURIComponent(sub)}
+                  class={twMerge(`flex px-2 py-1 text-[12px] font-light text-[#f0ece0] 
+                         hover:text-[#cdea19] hover:underline  ${$page.url.pathname === '/numpad' ? 'text-[#000]  hover:text-[#000]  ' : 'text-[#f0ece0] '}`)}
+                >
+                  {sub} <span class="text-[#ff4d00] uppercase pl-1 font-sans font-medium">NEW</span>
+                </a>
+                
+                {/if}
+              </li>
+							{/each}
+						</ul>
+					{/if}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				</li>
+			{:else if url == 'keyboard'}
+				<li>
+					<a
+						href={'/'}
+						class={twMerge(`bg text-[12px] font-light text-[#f0ece0] !not-italic 
+                   hover:text-[#cdea19] hover:underline  ${$page.url.pathname === '/numpad' ? 'text-[#000]  hover:text-[#000]  ' : 'text-[#f0ece0] '}`)}
+					>
+						{url}
+					</a>
+				</li>
+			{:else}
+				<li>
+					<a
+						href={'/' + url}
+						class={twMerge(`bg text-[12px] font-light text-[#f0ece0] !not-italic 
+                   hover:text-[#cdea19] hover:underline  ${$page.url.pathname === '/numpad' ? 'text-[#000]  hover:text-[#000]  ' : 'text-[#f0ece0] '}`)}
+					>
+						{url}
+					</a>
+				</li>
+			{/if}
+		{/each}
+	</ul>
+
+	<form action="" class="hidden gap-4 md:flex">
+		<input
+			type="email"
+			name="email"
+			class="h-10 max-w-48 rounded-4xl not-italic ring-0 outline-0
+             placeholder:text-[13px] placeholder:tracking-wider
              focus:border-none active:border-none"
-      placeholder="your email address"
-    />
-    <Button btn_text={'Get Updates'} />
-  </form>
+			placeholder="your email address"
+		/>
+		<Button btn_text={'Get Updates'} />
+	</form>
 </nav>
